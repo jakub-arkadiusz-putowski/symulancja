47cea63da799c203886eea90462e53116558c8f4
jakub-arkadiusz-putowski <jakputowski@gmail.com>
2026-03-17 23:06:33 +0100

    Update install_gazebo.sh

--- tools/install_gazebo.sh
+++ tools/install_gazebo.sh
@@ -1,8 +1,10 @@
-#!/usr/bash
+#!/usr/bin/bash
 
 #dodanie repo gz
 sudo apt update
 sudo apt install -y curl gnupg lsb-release
+
+#dodanie klucza gpg
 sudo curl -fsSL https://packages.osrfoundation.org/gazebo.gpg \
   -o /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 sudo apt update
