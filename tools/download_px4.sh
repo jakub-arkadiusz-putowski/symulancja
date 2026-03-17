6d2e155a0f945ffcba166f06df70f611a8cd6e89
jakub-arkadiusz-putowski <jakputowski@gmail.com>
2026-03-17 22:31:48 +0100

    Update and rename download_px4 to download_px4.sh

---
+++ tools/download_px4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#git clone https://github.com/PX4/PX4-Autopilot.git
+
+#instalka px4
+REPO_URL="https://github.com/PX4/PX4-Autopilot.git"
+TARGET_DIR="PX4-Autopilot"
+
+
+
+if [ -d "$TARGET_DIR" ]; then
+  echo "'$TARGET_DIR' juz istnieje"
+else
+  echo "instalka '$TARGET_DIR'"
+  git clone "$REPO_URL" "$TARGET_DIR"
+  echo "repo px4 siedzi w '$TARGET_DIR'"
+fi
