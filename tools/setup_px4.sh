c7a9fdde0b76d4d1faddbdfa4031f241127449c8
jakub-arkadiusz-putowski <jakputowski@gmail.com>
2026-03-17 23:08:14 +0100

    Update setup_px4.sh

--- tools/setup_px4.sh
+++ tools/setup_px4.sh
@@ -1,9 +1,12 @@
-#!/usr/bash
+#!/usr/bin/bash
 
 PX4_DIR="PX4-Autopilot"
+
+#ni ma katalogu
 if [ ! -d "$PX4_DIR" ]; then
   echo "ni ma katalogu '$PX4_DIR'"
   echo "wykonaj ./tools/download_px4.sh"
   exit 1
 fi
 
+echo 
