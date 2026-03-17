0dd0034a5d039329c86c55bf5604731912164692
jakub-arkadiusz-putowski <jakputowski@gmail.com>
2026-03-17 22:41:06 +0100

    Create install_gazebo.sh

---
+++ tools/install_gazebo.sh
@@ -0,0 +1,11 @@
+#!/usr/bash
+
+#dodanie repo gz
+sudo apt update
+sudo apt install -y curl gnupg lsb-release
+sudo curl -fsSL https://packages.osrfoundation.org/gazebo.gpg \
+  -o /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+sudo apt update
+
+#instalka gz
+sudo apt install -y gz-harmonic
