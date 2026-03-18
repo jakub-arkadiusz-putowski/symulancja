e776d6105c17738073a57009502d0ed304637231
jakub-arkadiusz-putowski <jakputowski@gmail.com>
2026-03-18 19:12:41 +0100

    Update download_px4.sh

--- tools/download_px4.sh
+++ tools/download_px4.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 PX4_DIR="$HOME/symulancja/PX4-Autopilot"
 PX4_URL="https://github.com/PX4/PX4-Autopilot.git"
 
-if [ ! -d "$PX4_DIR"]; then
+if [ ! -d "$PX4_DIR" ]; then
   git clone "$PX4_URL" --branch v1.14.3 --recursive "$PX4_DIR"
 fi
